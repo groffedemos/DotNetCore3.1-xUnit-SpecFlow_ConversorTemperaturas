b8498180f67efd9a97ebc5c6d64fb75e4220fbe1
Renato Groffe <renato.groffe@yahoo.com.br>
2020-09-05 10:27:37 -0300

    .NET Core 3.1 + SpecFlow + xUnit + Conversor Temperaturas
    .NET Core 3.1 + SpecFlow + xUnit + Conversor Temperaturas

---
+++ Temperatura.Especificacoes/ConvTemperaturasStepDefinition.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using TechTalk.SpecFlow;
+
+namespace Temperatura.Especificacoes
+{
+    [Binding]
+    public class ConversaoDeTemperaturasEmFahrenheitSteps
+    {
+        private double _temperaturaFahrenheit;
+        private double _temperaturaCelsius;
+        private double _temperaturaKelvin;
+        
+        [Given("que o valor da temperatura é de (.*) graus Fahrenheit")]
+        public void PreencherTemperaturaFahrenheit(double valorFahrenheit)
+        {
+            _temperaturaFahrenheit = valorFahrenheit;
+        }
+        
+        [When("eu solicitar a conversão desta teperatura")]
+        public void ProcessarConversao()
+        {
+            _temperaturaCelsius =
+                ConversorTemperatura.FahrenheitParaCelsius(
+                    _temperaturaFahrenheit);
+            _temperaturaKelvin =
+                ConversorTemperatura.FahrenheitParaKelvin(
+                    _temperaturaFahrenheit);
+        }
+        
+        [Then("o resultado da conversão para Celsius será de (.*) graus")]
+        public void ValidarResultadoCelsius(double valorCelsius)
+        {
+            Assert.Equal(valorCelsius, _temperaturaCelsius);
+        }
+
+        [Then("o resultado da conversão para Kelvin será de (.*) graus")]
+        public void ValidarResultadoKelvin(double valorKelvin)
+        {
+            Assert.Equal(valorKelvin, _temperaturaKelvin);
+        }
+    }
+}
---
+++ Temperatura/ConversorTemperatura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Temperatura
+{
+    public static class ConversorTemperatura
+    {
+        public static double FahrenheitParaCelsius(
+            double temperatura)
+        {
+            return Math.Round(
+                (temperatura - 32.0) / 1.8, 2);
+        }
+
+        public static double FahrenheitParaKelvin(
+             double temperatura)
+        {
+            return Math.Round(
+                FahrenheitParaCelsius(temperatura) + 273.15, 2);
+        }
+    }
+}
