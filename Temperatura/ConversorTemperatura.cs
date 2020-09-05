5ff09394c2b900bb33c3fdc7bce9d5933ba3e625
Renato Groffe <renato.groffe@yahoo.com.br>
2020-09-05 10:35:44 -0300

    Update ConversorTemperatura.cs

--- Temperatura/ConversorTemperatura.cs
+++ Temperatura/ConversorTemperatura.cs
@@ -7,15 +7,17 @@ namespace Temperatura
         public static double FahrenheitParaCelsius(
             double temperatura)
         {
-            return Math.Round(
-                (temperatura - 32.0) / 1.8, 2);
+            return (temperatura - 32.0) / 1.8;
+            //return Math.Round(
+            //    (temperatura - 32.0) / 1.8, 2);
         }
 
         public static double FahrenheitParaKelvin(
              double temperatura)
         {
-            return Math.Round(
-                FahrenheitParaCelsius(temperatura) + 273.15, 2);
+            return FahrenheitParaCelsius(temperatura) + 273.15;
+            //return Math.Round(
+            //    FahrenheitParaCelsius(temperatura) + 273.15, 2);
         }
     }
 }
